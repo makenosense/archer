a57f00e82337d7134cddedacd178de51b5e9e236
Nathan <nathan1108@163.com>
2021-03-04 13:43:30 +0800

    upload

--- src/archer/model/RepositorySearchEntry.java
+++ src/archer/model/RepositorySearchEntry.java
@@ -4,15 +4,15 @@ import org.tmatesoft.svn.core.SVNDirEntry;
 
 public class RepositorySearchEntry extends RepositoryDirEntry {
 
-    private final RepositoryPathNode dirPathNode;
+    private final RepositoryPathNode parentPathNode;
 
-    public RepositorySearchEntry(RepositoryPathNode dirPathNode, SVNDirEntry entry) {
+    public RepositorySearchEntry(RepositoryPathNode parentPathNode, SVNDirEntry entry) {
         super(entry);
-        this.dirPathNode = dirPathNode;
+        this.parentPathNode = parentPathNode;
     }
 
     @Override
     public String getPath() {
-        return dirPathNode.resolve(getName()).getPath();
+        return parentPathNode.resolve(getName()).getPath();
     }
 }
