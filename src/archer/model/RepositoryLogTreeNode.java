aa200962221caf2b8b6c0481698a91c6054d5d48
Nathan <nathan1108@163.com>
2021-03-08 21:10:09 +0800

    log

--- src/archer/model/RepositoryLogTreeNode.java
+++ src/archer/model/RepositoryLogTreeNode.java
@@ -12,4 +12,16 @@ public class RepositoryLogTreeNode {
 
         public boolean opened = false;
     }
+
+    public RepositoryLogTreeNode(String id, String parent, String type, String text) {
+        this(id, parent, type, text, false);
+    }
+
+    public RepositoryLogTreeNode(String id, String parent, String type, String text, boolean opened) {
+        this.id = id;
+        this.parent = parent;
+        this.type = type;
+        this.text = text;
+        this.state.opened = opened;
+    }
 }
